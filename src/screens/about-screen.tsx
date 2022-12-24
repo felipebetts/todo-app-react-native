619483d70f839b19202a50cd93e0d87951a03b6a
felipebetts <felipe.betts@gmail.com>
2022-12-24 01:19:05 -0300

    about screen

--- src/screens/about-screen.tsx
+++ src/screens/about-screen.tsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import { Box, Text, VStack } from 'native-base'
+import {
+  Box,
+  Text,
+  VStack,
+  ScrollView,
+  Icon,
+  Image,
+  useColorModeValue
+} from 'native-base'
+import { Feather } from '@expo/vector-icons'
+
+import AnimatedColorBox from '../components/animated-color-box'
+import Navbar from '../components/navbar'
+import Masthead from '../components/masthead'
+import LinkButton from '../components/link-button'
 
 const AboutScreen = () => {
   return (
-    <VStack flex={1}>
-      <Box>
-        <Text>About</Text>
-      </Box>
-    </VStack>
+    <AnimatedColorBox
+      flex={1}
+      bg={useColorModeValue('warmGray.50', 'warmGray.900')}
+    >
+      <Masthead
+        title="About this app"
+        image={require('../assets/masthead.png')}
+      >
+        <Navbar />
+      </Masthead>
+      <ScrollView
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        mt="-20px"
+        bg={useColorModeValue('warmGray.50', 'primary.900')}
+        pt="30px"
+        p={4}
+      >
+        <VStack flex={1} space={4}>
+          <Box alignItems="center">
+            {/* <Image /> */}
+            <Text>About</Text>
+          </Box>
+        </VStack>
+      </ScrollView>
+    </AnimatedColorBox>
   )
 }
 
